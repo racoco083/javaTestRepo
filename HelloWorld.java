09d793dea829ffd39e8cced244405cc7ac360ebe
Taehoon Yeo <uytr083@naver.com>
2023-11-28 00:27:04 +0900

    Update HelloWorld.java
    add 6

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello taehoon 1 2 3 4 5 world!");
+    System.out.println("Hello taehoon 1 2 3 4 5 6 world!");
   }
 }
